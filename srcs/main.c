7bcd9386626e5c22899c46b6486db52e125282f2
Prezcoder <fbouchar@student.42quebec.com>
2023-05-17 11:09:01 -0400

    test

--- srcs/main.c
+++ srcs/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: emlamoth <emlamoth@student.42.fr>          +#+  +:+       +#+        */
+/*   By: fbouchar <fbouchar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/03 09:39:22 by emlamoth          #+#    #+#             */
-/*   Updated: 2023/05/03 16:50:55 by emlamoth         ###   ########.fr       */
+/*   Updated: 2023/05/17 11:07:09 by fbouchar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 
 void	ft_execute(char *path, char **argv)
 {
+	// allo man
 	int id;
 	id = fork();
 	if (id == 0)
