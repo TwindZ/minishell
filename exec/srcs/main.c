bb068e0d1fe27e0a9b0c52f07a58b3e9595c105b
TwindZ <emmanuel.lamothe@gmail.com>
2023-05-18 16:40:23 -0400

    rien de special teste pipe

--- exec/srcs/main.c
+++ exec/srcs/main.c
@@ -6,62 +6,106 @@
 /*   By: emlamoth <emlamoth@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/03 09:39:22 by emlamoth          #+#    #+#             */
-/*   Updated: 2023/05/17 18:06:17 by emlamoth         ###   ########.fr       */
+/*   Updated: 2023/05/18 14:11:57 by emlamoth         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	ft_execute(char *path, char **argv, int fd)
+t_data *init_data()
 {
-	int id;
-	id = fork();
-	if (id == 0)
-	{
-		dup2(fd, STDOUT_FILENO);
-		execve(path, argv, NULL);
-		exit(EXIT_SUCCESS);
-	}
-	wait (NULL);
-}
-
-int	*ft_pipe()
-{
-	int *fd[2];
-
-	pipe(fd);
+	static t_data *data;
 	
+	if(!data)
+	{
+		data = malloc(sizeof(data));
+		
+	}
+	return (data)
 }
+// void	ft_execute(char *path, char **argv, int fd)
+// {
+// 	int id;
+// 	id = fork();
+// 	if (id == 0)
+// 	{
+		
+// 		execve(path, argv, NULL);
+// 		exit(EXIT_SUCCESS);
+// 	}
+// 	wait (NULL);
+// }
+
+// int	*ft_pipe()
+// {
+// 	int *fd[2];
+
+// 	pipe(fd);
+	
+// }
 
 int main()
 {
-	t_data *data;
-	
-	char **envarr;
-	int i;
-	int pipe;
-	char *argv[] = {"ls", "-la", NULL};
+	t_data data;
+	int fd;
+	// int ret;
+	int id ;
+	char *argv[] = {"ls", "-la", "-la", "-la", "-la", "-la", "/bin", NULL};
 	char path[] = "/bin/ls";
-	char *argv2[] = {"cat", NULL};
+	char *argv2[] = {"allo", "-e", NULL};
 	char path2[] = "/bin/cat";
-	pipe = 0;
-	i = 0;
-	ft_printf("%d\n", access("/bin/ls", X_OK));
-	ft_printf("%s\n", getcwd(NULL, 0));
-	ft_printf("%d\n", chdir("./srcs"));
+	int pfd[2];
+	
+	pipe(pfd);
+	data.pfdr = pfd[0];
+	data.pfdw = pfd[1];
+	
+	fd = open("allo.txt", O_RDWR);
+	
+	// ft_printf("%d\n", fd);
+	// dup2(fd, pfd);
+	id = fork();
+	if(id == 0)
+	{
+		dup2(pfd[1], STDOUT_FILENO);
+		execve(path, argv, NULL);
+	}
+	id = fork();
+	if(id == 0)
+	{
+		dup2(pfd[0], STDIN_FILENO);
+		execve(path2, argv2, NULL);
+	}
+	close(data.pfdr);
+	close(data.pfdw);
+	// ft_printf("%d\n", fd);
+	// ft_printf("%d\n", ret);
+	// ft_printf("%d\n", STDIN_FILENO);
+	// t_data *data;
+	
+	// char **envarr;
+	// int i;
+	// int pipe;
+	// char *argv[] = {"ls", "-la", NULL};
+	// char path[] = "/bin/ls";
+	// char *argv2[] = {"cat", NULL};
+	// char path2[] = "/bin/cat";
+	// pipe = 0;
+	// i = 0;
+	// ft_printf("%d\n", access("/bin/ls", X_OK));
+	// ft_printf("%s\n", getcwd(NULL, 0));
+	// ft_printf("%d\n", chdir("./srcs"));
 	ft_printf("%s\n", getenv("PATH"));
 	envarr = ft_split(getenv("PATH"),':');
-	chdir("/usr/bin/");
-	ft_printf("%s\n", getcwd(NULL, 0));
-	if(pipe == 0)
-		ft_execute(path, argv, STDOUT_FILENO);
-	else
-	{	
-		
-		ft_execute(path, argv);
-	}
-	ft_printf("apres execute #1\n");
-	
+	// chdir("/usr/bin/");
+	// ft_printf("%s\n", getcwd(NULL, 0));
+	// if(pipe == 0)
+	// 	ft_execute(path, argv, STDOUT_FILENO);
+	// else
+	// {	
+	// 	ft_execute(path, argv);
+	// }
+	// ft_printf("apres execute #1\n");
 
 	return(0);
 }
--- exec/srcs/minishell.h
+++ exec/srcs/minishell.h
@@ -6,7 +6,7 @@
 /*   By: emlamoth <emlamoth@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/01 16:23:39 by emlamoth          #+#    #+#             */
-/*   Updated: 2023/05/17 18:29:15 by emlamoth         ###   ########.fr       */
+/*   Updated: 2023/05/18 14:37:18 by emlamoth         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,27 @@
 # define READ_END 0
 # define WRITE_END 1
 
-typedef struct s_lst
+typedef struct s_ltkn
 {
-	char			*cmd;
-	struct s_cmd	*next;
-}				t_lst;
+	char			*token;
+	struct s_ltkn	*next;
+}				t_ltkn;
+
+typedef struct s_lcmd
+{
+	char			*cmdpath;
+	char			**argv;
+	int				builtin;
+	struct s_lcmd	*next;
+}				t_lcmd;
 
 typedef struct s_data
 {
-	t_lst	cmd;
+	t_ltkn 	ltkn;
+	t_lcmd	lcmd;
+	char	**path;
+	int		pfdw;
+	int		pfdr;
 }				t_data;
 
 #include <term.h>
