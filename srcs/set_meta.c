2943cc60d9db660fc14b6eb47aa57d8158484299
TwindZ <emmanuel.lamothe@gmail.com>
2023-06-29 15:18:40 -0400

    uptade gestion des free

--- srcs/set_meta.c
+++ srcs/set_meta.c
@@ -6,7 +6,7 @@
 /*   By: emlamoth <emlamoth@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/21 16:33:30 by emlamoth          #+#    #+#             */
-/*   Updated: 2023/06/29 13:17:49 by emlamoth         ###   ########.fr       */
+/*   Updated: 2023/06/29 15:06:47 by emlamoth         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,29 @@
 
 void	set_out_trunk(t_data *data, char **arg)
 {
-	free(arg[data->i++]);
+	free(arg[data->i]);
+	arg[data->i] = NULL;
+	data->i++;
 	if (arg[data->i] && is_meta(data, arg) == 0 && ft_strncmp(arg[data->i], "|\0", 2))
 	{
 		open_outfile(data, arg[data->i], 0); //TODO si fichier qui suis est un token besoin d'erreur
 		data->temp_out_mod = 1;
 		if (data->temp_outfile)
+		{
 			free(data->temp_outfile);
+			data->temp_outfile = NULL;
+		}
 		data->temp_outfile = arg[data->i];
 	}
 	else
 	{
 		if (data->temp_outfile)
+		{	
 			free(data->temp_outfile);
+			data->temp_outfile = NULL;
+		}
 		free(arg[data->i]);
+		arg[data->i] = NULL;
 		ft_putstr_fd("Minishell: syntax error near unexpected token\n", 2);
 		return ;
 	}
@@ -35,13 +44,18 @@ void	set_out_trunk(t_data *data, char **arg)
 
 void	set_out_apend(t_data *data, char **arg)
 {
-	free(arg[data->i++]);
+	free(arg[data->i]);
+	arg[data->i] = NULL;
+	data->i++;
 	if (arg[data->i] && is_meta(data, arg) == 0 && ft_strncmp(arg[data->i], "|\0", 2))
 	{
 		open_outfile(data, arg[data->i], 0); //TODO si fichier qui suis est un token besoin d'erreur
 		data->temp_out_mod = 2;
 		if (data->temp_outfile)
+		{
 			free(data->temp_outfile);
+			data->temp_outfile = NULL;
+		}
 		data->temp_outfile = arg[data->i];
 	}
 	else
@@ -56,10 +70,14 @@ void	set_infile(t_data *data, char **arg)
 	int	fd;
 	
 	fd = 0;
-	free(arg[data->i++]);
+	free(arg[data->i]);
+	arg[data->i] = NULL;
+	data->i++;
 	if(data->temp_in_mod == -1)
 	{
-		free (arg[data->i++]);
+		free(arg[data->i]);
+		arg[data->i] = NULL;
+		data->i++;
 		return;
 	}
 	if (arg[data->i] && is_meta(data, arg) == 0 && ft_strncmp(arg[data->i], "|\0", 2))
@@ -71,7 +89,9 @@ void	set_infile(t_data *data, char **arg)
 			ft_putstr_fd(arg[data->i], 2);
 			ft_putstr_fd(": No such file or directory\n", 2);
 			data->temp_in_mod = -1;
-			free (arg[data->i++]);
+			free (arg[data->i]);
+			arg[data->i] = NULL;
+			data->i++;
 		}
 		else
 		{
@@ -90,7 +110,9 @@ void	set_infile(t_data *data, char **arg)
 
 void	set_heredoc(t_data *data, char **arg)
 {
-	free(arg[data->i++]);
+	free(arg[data->i]);
+	arg[data->i] = NULL;
+	data->i++;
 	if (arg[data->i] && is_meta(data, arg) == 0 && ft_strncmp(arg[data->i], "|\0", 2))
 	{
 		data->temp_in_mod = 2;
