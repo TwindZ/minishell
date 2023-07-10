8de129cb4b6820931324ceec988b02648bee7df8
TwindZ <emmanuel.lamothe@gmail.com>
2023-07-10 17:36:59 -0400

    normining

--- srcs/signal.c
+++ srcs/signal.c
@@ -6,7 +6,7 @@
 /*   By: emlamoth <emlamoth@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/27 16:23:51 by emlamoth          #+#    #+#             */
-/*   Updated: 2023/07/10 17:32:16 by emlamoth         ###   ########.fr       */
+/*   Updated: 2023/07/10 17:36:08 by emlamoth         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,26 +19,15 @@ void	sig_handler(int sig)
 	data = ft_init_data(NULL);
 	if(sig == SIGINT)
 	{
+		ft_putstr_fd("\n", STDOUT_FILENO);
+		rl_replace_line("", 0);
+		rl_on_new_line();
 		if(data->hdprocess)
-		{
-			rl_replace_line("", 0);
-			ft_putstr_fd("\n", STDOUT_FILENO);
-			rl_on_new_line();
 			exit(45);
-		}
 		else if (data->exeprocess)
-		{
-			rl_replace_line("", 0);
-			ft_putstr_fd("\n", STDOUT_FILENO);
-			rl_on_new_line();
-		}	
+			;
 		else
-		{
-			ft_putchar_fd('\n', STDOUT_FILENO);
-			rl_replace_line("", 0);
-			rl_on_new_line();
 			rl_redisplay();
-		}	
 	}
 	mini_reset(data);
 }
